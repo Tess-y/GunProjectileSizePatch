eaed6bcff0121e424ad72dac962a9f7b98d305d8
Tess <tess@tessyac.com>
2022-06-05 08:37:59 -0400

    Add project files.

---
+++ patch.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace GunProjectileSizePatch
+{    // Declares our Mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our Mod Is associated with
+    [BepInProcess("Rounds.exe")]
+    public class Main : BaseUnityPlugin
+    {
+        private const string ModId = "com.root.projectile.size.patch";
+        private const string ModName = "ProjectileSizePatch";
+        public const string Version = "1.0.0"; // What version are we On (major.minor.patch)?
+        void Awake()
+        {
+            // Use this to call any harmony patch files your Mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+
+        }
+    }
+
+    [Serializable]
+    [HarmonyPatch(typeof(Gun), "ApplyPlayerStuff")]
+    public class Patch
+    {
+        private static void Prefix(Gun __instance, GameObject obj)
+        {
+            foreach (var t in obj.GetComponentsInChildren<TrailRenderer>())
+            {
+                t.startWidth *= (1 + __instance.projectileSize);
+            }
+        }
+    }
+}
